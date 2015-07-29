ca801c883838c4b1a6331e285847e02234b53173
boggdan95 <bandabog@gmail.com>
2015-07-29 14:21:43 -0600

    Programa v1.

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -1,7 +1,66 @@
 import java.io.FileNotFoundException;
 
-public interface Calculadora {
-	public void readFile(String file) throws FileNotFoundException;
-	public <P> P calcular();
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+/**
+ *
+ * @author Admin
+ */
+public class Calculadora implements ADTCalculadora{
+    
+    private String datosPosfix; 
+    private Double Resultado;
+    private Stack<Double> numeros= new Stack<Double>();
+   
+    
+    public void readFile(String file) throws FileNotFoundException {
+        // TODO Auto-generated method stub
+        // tiene que tener el valor: file = "datos.txt"
+        FileReader fr = new FileReader(file);
+        BufferedReader bf = new BufferedReader(fr);
 
+        try {
+                datosPosfix = bf.readLine();
+        } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+        }
+        
+        System.out.println(datosPosfix);		
+	
+    }
+    
+    public double calcular(){
+	char[] cadenaChar = datosPosfix.toCharArray();
+
+	for (int i = 0; i < datosPosfix.length(); i++){
+		if (cadenaChar[i]== '+'){
+                    numeros.push(numeros.pop()+numeros.pop());
+		}
+                else if (cadenaChar[i]== '-'){
+                    numeros.push(numeros.pop()-numeros.pop());
+		}
+                else if (cadenaChar[i]== '*'){
+                    numeros.push(numeros.pop()*numeros.pop());
+		}            
+                else if (cadenaChar[i]== '/'){
+                    double x =numeros.pop();
+                    double y =numeros.pop();
+                    numeros.push(y/x);
+		}
+                else if (cadenaChar[i]== ' '){
+                    ;
+		}else {
+                    numeros.push(Double.parseDouble(""+cadenaChar[i]));
+                            }
+	}          
+        Resultado = numeros.pop();
+        
+        return Resultado;
+        
+    }
 }
+
+ 
--- src/Stack.java
+++ src/Stack.java
@@ -1,11 +1,46 @@
 
-public interface Stack {
-	
-	public void empty();
-	public boolean isEmpty();
-	public <E> void push(E x) ;
-	public <E> E pop() throws Exception;
-	public int size();
-	public <E> E peek() throws Exception;
-	
+import java.util.Vector;
+
+
+/**
+ * @author Boggdan Barrientos, Jose Corona, Julio Dieguez
+ *
+ * @param <E>
+ */
+public class Stack<E> implements ADTStack<E>{
+    
+    protected Vector<E> pila;
+    
+    public Stack(){
+        pila = new Vector<E>();
+    }
+    
+    public void push(E item){
+        pila.add(item);
+    }
+    
+    public E pop(){
+
+        return pila.remove(size()-1);
+    	
+    }
+    
+    public E peek(){
+        return pila.get(size() - 1);
+    }
+    
+    public boolean isEmpty(){
+        return size() == 0;
+    }
+    
+    public int size(){
+        return pila.size();
+    }
+    /**
+    *Borra todos los elementos de la pila
+    *
+    */
+    public void empty(){
+        pila.clear();
+    }
 }
