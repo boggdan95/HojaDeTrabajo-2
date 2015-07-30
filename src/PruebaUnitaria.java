287c03ddc5dfa27b44a70cb7a89ac54e85385ced
boggdan95 <bandabog@gmail.com>
2015-07-30 04:30:11 +0000

    Se agrego prueba unitaria del metodo de size()

---
+++ src/PruebaUnitaria.java
@@ -0,0 +1,15 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PruebaUnitaria {
+	Stack tester = new Stack();
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	int resultado = tester.size();
+	assertTrue(resultado == 0);
+	
+	}
+
+}
